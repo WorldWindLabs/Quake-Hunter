8821025aecd7394f6929231859be7051f4f69508
Miguel Del Castillo <migueldelcastillo@live.com>
2016-09-06 21:58:42 -0700

    Added slabs from USGS. Layer controller still with bugs

--- Control.js
+++ Control.js
@@ -15,7 +15,8 @@ define(['./Circle',
         './TectonicPlateLayer',
         './WorldPoint',
         './Draw',
-        './MetadataDisplay'],
+        './MetadataDisplay',
+        './DataGrid'],
     function (Circle,
               Cylinder,
               LayerManager,
@@ -29,7 +30,9 @@ define(['./Circle',
               TectonicPlateLayer,
               WorldPoint,
               Draw,
-              Metadata) {
+              Metadata,
+              DataGrid,
+              USGSSlabs) {
 
         "use strict";
 
@@ -54,6 +57,7 @@ define(['./Circle',
         // }
 
         var Control = function () {
+            //this.layersPanel = new LayersPanel(wwd);
             var earthquakes =  new USGS(wwd, this);
             var metadata = new Metadata(this);
             this.handler = new Draw(wwd, metadata, this);
@@ -147,11 +151,127 @@ define(['./Circle',
                 wwd.addEventListener("mousemove", myControl.handler.Drawer);
             };
 
+            // Configure the USGS earthquake slab layers.
+            this.loadSlabData("CAS", "cascadia_slab1.0_clip.xyz", 401, WorldWind.Color.YELLOW);
+            this.loadSlabData("SOL", "sol_slab1.0_clip.xyz", 1001, WorldWind.Color.YELLOW);
+            this.loadSlabData("MEX", "mex_slab1.0_clip.xyz", 1251, WorldWind.Color.CYAN);
+            this.loadSlabData("ALU", "alu_slab1.0_clip.xyz", 2451, WorldWind.Color.MAGENTA);
+
+            // Establish the shapes and the controllers to handle picking.
+            this.setupPicking();
+
+        };
+
+        Control.prototype.loadSlabData = function (name, dataFile, width, color) {
+            var dataLocation = "http://worldwindserver.net/webworldwind/data/usgs/",
+                url = dataLocation + dataFile;
+
+            var xhr = new XMLHttpRequest();
+
+            xhr.open("GET", url, true);
+            xhr.onreadystatechange = (function () {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        var dataGrid = new DataGrid(xhr.responseText);
+                        this.addGridToWorldWindow(name, dataGrid, color);
+                    }
+                    else {
+                        Logger.log(Logger.LEVEL_WARNING,
+                            "Slab data retrieval failed (" + xhr.statusText + "): " + url);
+                    }
+                }
+            }).bind(this);
+
+            xhr.onerror = function () {
+                Logger.log(Logger.LEVEL_WARNING, "Slab data retrieval failed: " + url);
+            };
+
+            xhr.ontimeout = function () {
+                Logger.log(Logger.LEVEL_WARNING, "Slab data retrieval timed out: " + url);
+            };
+
+            xhr.send(null);
+        };
+
+        Control.prototype.addGridToWorldWindow = function (name, dataGrid, color) {
+            var meshLayer = new WorldWind.RenderableLayer();
+            meshLayer.displayName = name;
+            wwd.addLayer(meshLayer);
+
+            var meshAttributes = new WorldWind.ShapeAttributes(null);
+            meshAttributes.drawOutline = false;
+            meshAttributes.outlineColor = WorldWind.Color.BLUE;
+            meshAttributes.interiorColor = color;
+            meshAttributes.applyLighting = true;
+
+            var highlightAttributes = new WorldWind.ShapeAttributes(meshAttributes);
+            highlightAttributes.outlineColor = WorldWind.Color.WHITE;
+
+            var indices = dataGrid.findTriangles();
+            var splitShapes = WorldWind.TriangleMesh.split(dataGrid.positions, indices, null, null);
+
+            for (var i = 0; i < splitShapes.length; i++) {
+                var mesh = new WorldWind.TriangleMesh(splitShapes[i].positions, splitShapes[i].indices, meshAttributes);
+                mesh.altitudeMode = WorldWind.ABSOLUTE;
+                mesh.highlightAttributes = highlightAttributes;
+                mesh.dataGrid = dataGrid;
+                meshLayer.addRenderable(mesh);
+            }
+
+            this.layersPanel.synchronizeLayerList();
+            wwd.redraw();
+        };
+
+        Control.prototype.setupPicking = function () {
+            this.screenText = new WorldWind.ScreenText(new WorldWind.Offset(WorldWind.OFFSET_PIXELS, 0, WorldWind.OFFSET_PIXELS, 0), " ");
+            this.screenText.attributes = new WorldWind.TextAttributes();
+            this.screenText.attributes.offset = new WorldWind.Offset(WorldWind.OFFSET_FRACTION, 0, WorldWind.OFFSET_FRACTION, 0);
+
+            this.textLayer = new WorldWind.RenderableLayer();
+            this.textLayer.hide = true;
+            this.textLayer.enabled = false;
+            this.textLayer.addRenderable(this.screenText);
+            wwd.addLayer(this.textLayer);
+
+            var handlePick = (function (o) {
+                var pickPoint = wwd.canvasCoordinates(o.clientX, o.clientY);
+
+                this.textLayer.enabled = false;
+                wwd.redraw();
+
+                var pickList = wwd.pick(pickPoint);
+                if (pickList.objects.length > 0) {
+                    for (var p = 0; p < pickList.objects.length; p++) {
+                        var pickedObject = pickList.objects[p];
+                        if (pickedObject.userObject instanceof WorldWind.TriangleMesh) {
+                            if (pickedObject.position) {
+                                var latitude = pickedObject.position.latitude,
+                                    longitude = pickedObject.position.longitude,
+                                    altitude = pickedObject.userObject.dataGrid.lookupValue(latitude, longitude);
+                                if (altitude !== null) {
+                                    this.screenText.screenOffset.x = pickPoint[0];
+                                    this.screenText.screenOffset.y = wwd.viewport.width - pickPoint[1];
+                                    this.screenText.text = Math.floor(Math.abs(altitude) / 1000).toString() + " Km";
+                                    this.textLayer.enabled = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }).bind(this);
+
+            // Listen for mouse moves and highlight the placemarks that the cursor rolls over.
+            wwd.addEventListener("mousemove", handlePick);
+
+            // Listen for taps on mobile devices and highlight the placemarks that the user taps.
+            var tapRecognizer = new WorldWind.TapRecognizer(wwd, handlePick);
         };
 
         var viewControlsLayer = new WorldWind.ViewControlsLayer(wwd);
         viewControlsLayer.alignment = new WorldWind.Offset(WorldWind.OFFSET_FRACTION, 0.25, WorldWind.OFFSET_FRACTION, 0);
         viewControlsLayer.placement = new WorldWind.Offset(WorldWind.OFFSET_FRACTION, 0.25, WorldWind.OFFSET_FRACTION, 0);
+
+        //var layersPanel = new LayersPanel(wwd);
 
         var layers = [
             {layer: new WorldWind.BMNGLayer(), enabled: true},
